0e35575dcc136fcf7140bcb5c4e0c9342d534eca
H-Emanuel <emanuelvperez2000@gmail.com>
2024-03-12 10:07:22 -0300

    servidor

--- core/settings.py
+++ core/settings.py
@@ -76,27 +76,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'transparencia_AU',
-         'USER': 'postgres',
-         'PASSWORD': '1234',
-         'HOST': 'localhost',
-         'PORT': '5432',
-     }
- }
-
 # DATABASES = {
-#     'default': {
+#      'default': {
 #          'ENGINE': 'django.db.backends.postgresql',
 #          'NAME': 'transparencia_AU',
-#          'USER': 'transparencia_AU',
-#          'PASSWORD': 'transparencia2024',
+#          'USER': 'postgres',
+#          'PASSWORD': '1234',
 #          'HOST': 'localhost',
 #          'PORT': '5432',
 #      }
 #  }
+
+DATABASES = {
+    'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'transparencia_AU',
+         'USER': 'transparencia_AU',
+         'PASSWORD': 'transparencia2024',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
+ }
 
 
 # Password validation
