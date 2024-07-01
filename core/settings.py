c5be1832e89f460e5f323baad459639a4dc98cb0
H-Emanuel <emanuelvperez2000@gmail.com>
2024-07-01 17:14:57 -0400

    CAMBIO_QUE_FUNCIONE

--- core/settings.py
+++ core/settings.py
@@ -77,27 +77,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'transparencia_au',
-         'USER': 'postgres',
-         'PASSWORD': '1234',
-         'HOST': 'localhost',
-         'PORT': '5432',
-     }
- }
-
 # DATABASES = {
-#     'default': {
+#      'default': {
 #          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'transparencia_au',
-#          'USER': 'transparencia_au',
-#          'PASSWORD': 'transparencia2024',
+#          'NAME': 'transparencia_AU',
+#          'USER': 'postgres',
+#          'PASSWORD': '1234',
 #          'HOST': 'localhost',
 #          'PORT': '5432',
 #      }
 #  }
+
+DATABASES = {
+    'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'transparencia_au',
+         'USER': 'transparencia_au',
+         'PASSWORD': 'transparencia2024',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
+ }
 
 
 # Password validation
